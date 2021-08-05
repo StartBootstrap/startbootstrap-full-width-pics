f9e71d73d452b1cabcdc8545f6a2194d75edb7d9
David Miller <hello@davidmiller.io>
2021-08-05 16:02:43 -0400

    Bootstrap 5.1.0 Update

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Full Width Pics v5.0.2 (https://startbootstrap.com/template/full-width-pics)
+* Start Bootstrap - Full Width Pics v5.0.3 (https://startbootstrap.com/template/full-width-pics)
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-full-width-pics/blob/master/LICENSE)
 */
